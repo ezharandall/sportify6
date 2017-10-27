c28f45438d4ff548dd03f9bd24a8e22480c55076
Ezha Randall <ezha.randall@gmail.com>
2017-10-27 16:11:07 -0400

    edited login

--- src/login/Login.js
+++ src/login/Login.js
@@ -4,15 +4,15 @@ import ReactSignupLoginComponent from 'react-signup-login-component';
 const Login = (props) => {
     const signupWasClickedCallback = (data) => {
       console.log(data);
-      alert('Signup callback, see log on the console to see the data.');
+      alert('Access Granted!');
     };
     const loginWasClickedCallback = (data) => {
       console.log(data);
-      alert('Login callback, see log on the console to see the data.');
+      alert('Access Granted!');
     };
     const recoverPasswordWasClickedCallback = (data) => {
       console.log(data);
-      alert('Recover password callback, see log on the console to see the data.');
+      alert('Access Granted!');
     };
     return (
         <div>
